41a7afc1168910fc5d4c55a161a1e16561b41d48
Shashank <87185629+SHASHANKPAL301@users.noreply.github.com>
2026-08-03 18:37:25 +0530

    Time: 12 ms (28.15%) | Memory: 50.3 MB (38.55%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -1,47 +1,41 @@
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
-        int low = 0;
-        int high = 0;
-        
-        // 1. Find the correct search space boundaries
-        for (int weight : weights) {
-            low = Math.max(low, weight); // Min capacity = heaviest single package
-            high += weight;              // Max capacity = sum of all packages
+        int low=0;
+        int high=0;
+        for(int weight:weights){
+            low=Math.max(low, weight);
+            high+=weight;
         }
-        
-        int ans = high;
-        
-        // 2. Binary Search
-        while (low <= high) {
-            int mid = low + (high - low) / 2; // 'mid' is our guessed capacity
-            
-            // 3. Test the guessed capacity
-            if (canShipInTime(weights, days, mid)) {
-                ans = mid;       // This capacity works, record it!
-                high = mid - 1;  // But try to find an even smaller capacity
-            } else {
-                low = mid + 1;   // It took too many days, we need a larger capacity
+        int ans=high;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(check(weights,days,mid)){
+                high=mid-1;
+                ans=mid;
+            }
+            else{
+                low=mid+1;
+
             }
         }
         return ans;
     }
-    
-    // Helper function to simulate loading the ship
-    private boolean canShipInTime(int[] weights, int targetDays, int capacity) {
-        int daysNeeded = 1; // We always start on day 1
-        int currentLoad = 0;
-        
-        for (int weight : weights) {
-            // If adding this package exceeds capacity, ship it the next day
-            if (currentLoad + weight > capacity) {
-                daysNeeded++;
-                currentLoad = weight; // Start the new day with this package
-            } else {
-                currentLoad += weight; // Otherwise, keep adding to today's load
+    public boolean check(int[] weights,int days,int capacity){
+        int reqDays=1;
+        int cap=0;
+        for(int weight:weights){
+            if(weight+cap > capacity){
+                cap=weight;
+                reqDays++;
+            }
+            else{
+                cap+=weight;
             }
         }
-        
-        // Return true if we finished within the allowed days
-        return daysNeeded <= targetDays; 
+        if(reqDays<=days){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
