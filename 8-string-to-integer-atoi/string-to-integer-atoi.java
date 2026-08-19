01981ef50875aad5781671fc42b33c5a25eb44a3
Shashank <87185629+SHASHANKPAL301@users.noreply.github.com>
2026-08-19 19:36:11 +0530

    Time: 1 ms (100.00%) | Memory: 43.5 MB (94.82%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,33 +1,36 @@
 class Solution {
     public int myAtoi(String s) {
+        boolean isNumber=false;
         int result=0;
-            int sign=1;
-            boolean hasStarted=false;
-        for(int i=0;i<s.length();i++){
+        int sign=1;
+        for(int i =0;i<s.length();i++){
             char ch = s.charAt(i);
-            
-            if(!hasStarted && ch==' '){
+            if(!isNumber && ch==' '){
                 continue;
             }
-            else if(!hasStarted && (ch=='-' || ch=='+')){
-                sign=(ch=='-') ? -1 :1;
-                hasStarted=true;
+            else if(!isNumber && (ch=='-'|| ch=='+')){
+                if(ch=='-'){
+                    sign=-1;
+                }
+                else{
+                    sign=1;
+                }
+                isNumber=true;
             }
-           
             else if(Character.isDigit(ch)){
-                hasStarted=true;
-                int b= ch -'0';
-                if (result > Integer.MAX_VALUE / 10 || (result == Integer.MAX_VALUE / 10 && b > 7)) {
-                    if (sign == 1) {
+                isNumber =true;
+                int b= ch-'0';
+                if(result>Integer.MAX_VALUE/10 || (result == Integer.MAX_VALUE/10 && b>7 )){
+                    if(sign==1){
                         return Integer.MAX_VALUE;
-                    } else {
+                    }
+                    else{
                         return Integer.MIN_VALUE;
                     }
                 }
                 result=result*10+b;
-                
             }
-            else{
+            else {
                 break;
             }
         }
